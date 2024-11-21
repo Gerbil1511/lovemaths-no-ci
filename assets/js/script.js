e47ea21521ded83c5dd1ed670fa30fed6ab688f0
Geraldine Edwards <gerbil1511@gmail.com>
2024-11-21 17:21:11 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// run game
+function runGame() {
+
+}
+
+// check answer
+function checkAnswer() {
+
+}
+
+// calculate correct answer
+function calculateCorrectSAnswer() {
+
+}
+
+// increment score
+function incrementScore() {
+
+}
+
+// incremet wrong answer
+function incrementWrongAnswer() {
+
+}
+
+// display addition q
+function displayAdditionQ() {
+
+}
+
+// display subtract q
+function displaySubtactQ() {
+
+}
+
+// display mulitply q
+function displayMultiplyQ() {
+
+}
+// display divide q
+function displayDivideQ() {
+
+}
