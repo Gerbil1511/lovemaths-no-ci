496ac6d71b16a4595c6b257fad16a7f89ff5d3d4
Geraldine Edwards <gerbil1511@gmail.com>
2024-11-22 10:32:39 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,23 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     let buttons = document.getElementsByTagName("button");
-
-//     for(let button of buttons) {
-//         button.addEventListener("click", function() {
-//             if (this.getAttribute("data-type") === "submit") {
-//                 alert("You clicked Submit!");
-//             } else {
-//                 let gameType = this.getAttribute("data-type");
-//                 alert(`You clicked ${gameType}`);
-//             }
-//         });
-//     }
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for(let button of buttons) {
         button.addEventListener("click", function() {
-            let gameType = this.getAttribute("data-type");
-            if (gameType) {
-                alert(`You clicked ${gameType}`);
-            } else if (this.getAttribute("data-type") === "submit") {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
+
+
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -37,11 +25,18 @@ document.addEventListener("DOMContentLoaded", function() {
  */ 
 
 // run game
-function runGame() {
+function runGame(gameType) {
     // creates two random numbers between 1 and 25 (even possibility of all numbers appearing)
     // using Math.ceil(Math.random() * 25) would do the same but 25 is less likely to appear
     let num1 = Math.floor(Math.random() *25) +1;
     let num2 = Math.floor(Math.random() *25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQ(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 // check answer
@@ -65,8 +60,10 @@ function incrementWrongAnswer() {
 }
 
 // display addition q
-function displayAdditionQ() {
-
+function displayAdditionQ(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 // display subtract q
